cb20ef06a0d53d21befe8dc86f1db4d148179b3e
Pranav Raja <pranav.raja@mi9.com.au>
2013-05-21 17:45:38 +1000

    Initial commit of regex refactoring
    The refactor package contains some tools for replacing stuff in strings,
    working with patches and confirmation. Hopefully I can build this into
    something useful.

---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"./src/refactor"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+)
+
+func walk(root string, filePaths chan<- string) {
+	defer close(filePaths)
+	entries, err := ioutil.ReadDir(root)
+	if err != nil {
+		return
+	}
+	for _, file := range entries {
+		if strings.HasPrefix(file.Name(), ".") {
+			continue // Ignore hidden files
+		}
+		fullPath := path.Join(root, file.Name())
+		if file.IsDir() {
+			nestedFilePaths := make(chan string)
+			go walk(fullPath, nestedFilePaths)
+			for f := range nestedFilePaths {
+				filePaths <- f
+			}
+		} else {
+			if !strings.Contains(file.Name(), ".") {
+				continue // Ignore binary files
+			}
+			filePaths <- fullPath
+		}
+	}
+}
+
+func patchAll(filenames <-chan string, find *regexp.Regexp, replace string, patches chan<- refactor.Patch, proceed <-chan bool) {
+	defer close(patches)
+	for filename := range filenames {
+		f, err := os.Open(filename)
+		if err != nil {
+			continue
+		}
+		contents, err := ioutil.ReadAll(f)
+		f.Close()
+		if err != nil {
+			continue
+		}
+		filePatches := make(chan refactor.Patch)
+		patcherCanProceed := make(chan bool)
+		fileResult := make(chan string)
+		go refactor.Patcher(string(contents), find, replace, filePatches, patcherCanProceed, fileResult)
+		for patch := range filePatches {
+			patch.Filename = filename
+			patches <- patch
+			patcherCanProceed <- <-proceed
+		}
+		r, changed := <-fileResult
+		if changed {
+			ioutil.WriteFile(filename, []byte(r), 0) // Assume file already exists
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) <= 2 {
+		println("Usage: refactor find replace")
+		return
+	}
+	find := regexp.MustCompile(os.Args[1])
+	replace := os.Args[2]
+	patches := make(chan refactor.Patch)
+	proceed := make(chan bool)
+
+	paths := make(chan string)
+	go walk(".", paths)
+	go patchAll(paths, find, replace, patches, proceed)
+	var canProceed bool
+	for p := range patches {
+		fmt.Printf("%v\n", p)
+		if !canProceed {
+			fmt.Printf("Continue? (y/n[default]): ")
+			var input rune
+			_, err := fmt.Scanf("%c", &input)
+			if input == 'y' {
+				canProceed = true
+			}
+			if err != nil {
+				fmt.Printf("%v", err)
+				return
+			}
+			if !canProceed {
+				return
+			}
+		}
+		proceed <- true
+	}
+}
---
+++ src/refactor/refactor.go
@@ -0,0 +1,75 @@
+package refactor
+
+import (
+	"fmt"
+	"strings"
+)
+
+func outerLinesForSubstring(str string, start int, end int) (startOfFirstLine int, endOfLastLine int) {
+	startOfFirstLine = strings.LastIndex(str[0:start], "\n")
+	if startOfFirstLine == -1 {
+		startOfFirstLine = 0
+	} else {
+		startOfFirstLine += 1 // Go past that newline
+	}
+	endOfLastLine = strings.Index(str[end:], "\n")
+	if endOfLastLine == -1 {
+		endOfLastLine = len(str)
+	} else {
+		endOfLastLine += end // Add the rest of the string's length
+	}
+	return
+}
+
+type replacer interface {
+	ReplaceAllString(src, repl string) string
+}
+
+// Returns a before/after view of the surrounding lines when find/replace is done
+func patchForSubstring(str string, start int, end int, find replacer, replace string) (newStr string, before string, after string) {
+	replaced := find.ReplaceAllString(str[start:end], replace)
+	originalStart, originalEnd := outerLinesForSubstring(str, start, end)
+	before = str[originalStart:originalEnd]
+	newStr = str[0:start] + replaced + str[end:]
+	newStart, newEnd := outerLinesForSubstring(newStr, start, start+len(replaced))
+	after = newStr[newStart:newEnd]
+	return
+}
+
+type Patch struct {
+	Filename string
+	Before   string
+	After    string
+}
+
+func (p Patch) String() string {
+	return fmt.Sprintf("%s:\n\t%s => %s\n", p.Filename, p.Before, p.After)
+}
+
+type finder interface {
+	replacer
+	FindStringIndex(str string) []int
+}
+
+func Patcher(str string, find finder, replace string, patches chan<- Patch, proceed <-chan bool, result chan<- string) {
+	defer close(result)
+	var before, after string
+	var progress int
+	match := find.FindStringIndex(str[progress:])
+	if match == nil {
+		close(patches)
+		return
+	}
+	for match != nil {
+		str, before, after = patchForSubstring(str, progress+match[0], progress+match[1], find, replace)
+		progress = match[1] - len(before) + len(after)
+		patches <- Patch{Before: before, After: after}
+		canProceed := <-proceed
+		if !canProceed {
+			break
+		}
+		match = find.FindStringIndex(str[progress:])
+	}
+	close(patches)
+	result <- str
+}
