d1770a0f3333250f3c4cce8d0ef3fb464bb57e94
Pranav Raja <pranav.raja@mi9.com.au>
2013-06-20 14:43:38 +1000

    Use path/filepath.Walk builtin

--- main.go
+++ main.go
@@ -1,42 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/ninemsn/refactor/confirm"
 	"github.com/ninemsn/refactor/patch"
 	"github.com/vrischmann/termcolor"
 	"io"
-	"io/ioutil"
 	"os"
-	"path"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
-
-func walk(root string, suffix string, filePaths chan<- string) {
-	defer close(filePaths)
-	entries, err := ioutil.ReadDir(root)
-	if err != nil {
-		return
-	}
-	for _, file := range entries {
-		if strings.HasPrefix(file.Name(), ".") {
-			continue // Ignore hidden files
-		}
-		fullPath := path.Join(root, file.Name())
-		if file.IsDir() {
-			nestedFilePaths := make(chan string)
-			go walk(fullPath, suffix, nestedFilePaths)
-			for f := range nestedFilePaths {
-				filePaths <- f
-			}
-		} else {
-			if strings.HasSuffix(fullPath, suffix) {
-				filePaths <- fullPath
-			}
-		}
-	}
-}
 
 func prettyPrint(filename string, patch *patch.Patch, out io.Writer) {
 	fmt.Fprintf(out, "%s\n %s\n %s\n", termcolor.Colored(filename, termcolor.Cyan), termcolor.Colored("-"+patch.Before(), termcolor.Red), termcolor.Colored("+"+patch.After(), termcolor.Green))
@@ -46,23 +21,22 @@ func confirmPatch(filename string, p *patch.Patch, confirmation *confirm.Confirm
 	prettyPrint(filename, p, os.Stdout)
 	if confirmation.Next() {
 		return true
-	} else {
-		fmt.Printf("Continue? ([a]ll/[y]es/[n]o (default no): ")
-		var input string
-		_, err := fmt.Scanf("%s", &input)
-		if err != nil {
-			return false
-		}
-		switch input {
-		case "a":
-			confirmation.ConfirmAll()
-		case "y":
-			confirmation.ConfirmOnce()
-		default:
-			return false
-		}
-		return confirmation.Next()
 	}
+	fmt.Printf("Continue? ([a]ll/[y]es/[n]o (default no): ")
+	var input string
+	_, err := fmt.Scanf("%s", &input)
+	if err != nil {
+		return false
+	}
+	switch input {
+	case "a":
+		confirmation.ConfirmAll()
+	case "y":
+		confirmation.ConfirmOnce()
+	default:
+		return false
+	}
+	return confirmation.Next()
 }
 
 func main() {
@@ -74,25 +48,32 @@ func main() {
 	find := regexp.MustCompile(os.Args[2])
 	replace := []byte(os.Args[3])
 
-	paths := make(chan string)
-	go walk(".", suffix, paths)
 	confirmation := new(confirm.Confirmation)
-	for file := range paths {
-		patcher := patch.NewPatcher(file, find, replace)
-		err := patcher.Load()
+	filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			println(err)
-			return
+			return err
+		}
+		if strings.HasPrefix(info.Name(), ".") {
+			return nil
+		}
+		if !strings.HasSuffix(path, suffix) {
+			return nil
+		}
+		patcher := patch.NewPatcher(path, find, replace)
+		err = patcher.Load()
+		if err != nil {
+			return err
 		}
 		for p := patcher.Next(); p != nil; p = patcher.Next() {
-			canProceed := confirmPatch(file, p, confirmation)
+			canProceed := confirmPatch(path, p, confirmation)
 			if canProceed {
 				patcher.Accept(p)
 			} else {
 				patcher.Done()
-				return
+				return errors.New("refactor aborted by user")
 			}
 		}
 		patcher.Done()
-	}
+		return nil
+	})
 }
